9e6dd510b40d80d5c718f00779e2b9ba3d50f18d
Anastasia Korotkova <a.korotkova@yahoo.fr>
2025-11-21 15:12:10 +0100

    add types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import { TodosResponse } from "./types";
+
 (async () => {
   const res = await fetch("https://dummyjson.com/todos");
-  const data = await res.json();
+  const data: TodosResponse = await res.json();
 
-  console.log(data);
+  console.log("Total todos:", data.total);
+  for (const todo of data.todos) {
+    console.log(todo.id, todo.todo, todo.completed);
+  }
 })();
